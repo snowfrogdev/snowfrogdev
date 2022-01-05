8cbd533c3267e270fdf89ccbfa6a45587ca7f9d9
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2022-01-04 21:32:44 -0500

    (iter) Implement Iter.to*()

--- packages/iter/src/lib/iter.ts
+++ packages/iter/src/lib/iter.ts
@@ -1,17 +1,25 @@
-import { None, Some, Option } from "@snowfrog/option";
+import { None, Option, Some } from '@snowfrog/option';
 
 export class Iter<T> implements Iterable<T> {
-  private constructor(private iterator: Iterator<T>) {};
+  private constructor(private iterator: Iterator<T>) {}
   static from<T>(iterable: Iterable<T>): Iter<T> {
     return new Iter(iterable[Symbol.iterator]());
   }
 
+  [Symbol.iterator](): Iterator<T> {
+    return this.iterator;
+  }
+
   next(): Option<T> {
     const { done, value } = this.iterator.next();
-    return done ? new None() : new Some(value);	
-  };
+    return done ? new None() : new Some(value);
+  }
 
-  [Symbol.iterator](): Iterator<T> { 
-    return this.iterator;
+  toArray(): T[] {
+    return [...this];
+  }
+
+  toSet(): Set<T> {
+    return new Set(this);
   }
 }
---
+++ packages/iter/src/lib/to.spec.ts
@@ -0,0 +1,86 @@
+import { Iter } from './iter';
+
+describe('Iter', () => {
+  it('to Array using spread operator', () => {
+    const iter = Iter.from([1, 2, 3]);
+    const arr = [...iter];
+
+    expect(arr).toBeInstanceOf(Array);
+    expect(arr).toEqual([1, 2, 3]);
+  });
+
+  it('to Array using Iter.toArray()', () => {
+    const iter = Iter.from([1, 2, 3]);
+    const arr = iter.toArray();
+
+    expect(arr).toBeInstanceOf(Array);
+    expect(arr).toEqual([1, 2, 3]);
+  });
+
+  it('to Set using Set constructor', () => {
+    const iter = Iter.from([1, 2, 3]);
+
+    const expected = new Set();
+    expected.add(1);
+    expected.add(2);
+    expected.add(3);
+
+    const set = new Set(iter);
+
+    expect(set).toBeInstanceOf(Set);
+    expect(set).toEqual(expected);
+  });
+
+  it('to Set using Iter.toSet()', () => {
+    const iter = Iter.from([1, 2, 3]);
+
+    const expected = new Set();
+    expected.add(1);
+    expected.add(2);
+    expected.add(3);
+
+    const set = iter.toSet();
+
+    expect(set).toBeInstanceOf(Set);
+    expect(set).toEqual(expected);
+  });
+
+  it('to Map using Map constructor', () => {
+    const iter = Iter.from<[number, string]>([
+      [1, 'one'],
+      [2, 'two'],
+      [3, 'three'],
+    ]);
+
+    const expected = new Map();
+    expected.set(1, 'one');
+    expected.set(2, 'one');
+    expected.set(3, 'one');
+
+    const map = new Map(iter);
+
+    expect(map).toBeInstanceOf(Map);
+    expect(map).toEqual(expected);
+  });
+
+  xit('to Map using Iter.toMap()', () => {
+    // TODO: Fix this test and find a way to implement toMap() with
+    // geneic type T being unrestricted whil the map constructor requires
+    // T to be a tuple of [K, V]
+    /* const iter = Iter.from<[number, string]>([
+      [1, 'one'],
+      [2, 'two'],
+      [3, 'three'],
+    ]);
+
+    const expected = new Map();
+    expected.set(1, 'one');
+    expected.set(2, 'one');
+    expected.set(3, 'one');
+
+    const map = iter.toMap();
+
+    expect(map).toBeInstanceOf(Set);
+    expect(map).toEqual(expected); */
+  });
+});
