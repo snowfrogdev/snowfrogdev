e24e4ea6ba49530a4e8f56662cfe11c5a3b4449e
Philippe Vaillancourt <philippe@bamboosoft.ca>
2022-01-14 17:46:49 +0000

    (ngx-unleash-proxy-client-app) initial commit

---
+++ packages/ngx-unleash-proxy-client-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'snowfrog-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'ngx-unleash-proxy-client-app';
+}
